08d7614b29a23b4943fc948a15b37ed3d075895c
leobr <leobrionesherrera@gmail.com>
2020-08-28 13:19:58 -0500

    Form for editing house members:

--- src/houseMember.js
+++ src/houseMember.js
@@ -18,8 +18,23 @@ class HouseMember {
             el.innerHTML += `<td id="member-${this.id}-day-${counter}" class="text-center"></td>`
             counter++
         });
-        ul.innerHTML += `<li class="list-group-item" id="member-li-${this.id}">${this.name}\
-        <button type="button" class="button edit-btn-member">Edit</button>
-        <button type="button" class="button delete-btn-member">Delete</button></li>`
+        let li = document.createElement("li");
+        li.setAttribute("id", `member-li-${this.id}`);
+        li.classList.add("list-group-item");
+        li.innerHTML += `<section id="member-${this.id}-name-field">${this.name}</ section>
+        <button  class="button edit-btn-member">Edit</button>
+        <button  class="button delete-btn-member">Delete</button>`;
+        ul.appendChild(li);
+        let editBttn = document.querySelector(`#member-li-${this.id} .edit-btn-member`);
+        let deleteBttn = document.querySelector(`#member-li-${this.id} .delete-btn-member`);
+        editBttn.addEventListener("click", this.editMemberField);
+    }
+
+    editMemberField = (e) => {
+        let nameDiv = document.getElementById(`member-${this.id}-name-field`);
+        if (e.target.innerHTML === "Edit") {
+            e.target.innerHTML = "Save";
+            nameDiv.innerHTML =`<input type="text" value="${this.name}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">`
+        }
     }
 }
